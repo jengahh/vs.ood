a91f45f3aeb46af5b010ef997d2345522f1cb002
TaeYai <TaeYai123@gmail.com>
2021-09-24 21:40:05 +0700

    Update Options.hx

--- FNF VS.Ood KE 1.5.4/source/Options.hx
+++ FNF VS.Ood KE 1.5.4/source/Options.hx
@@ -128,16 +128,16 @@ class DownscrollOption extends Option
 	}
 
 	public override function press():Bool
-	{
-		!FlxG.save.data.downscroll;
-		display = updateDisplay();
-		return true;
-	}
-
-	private override function updateDisplay():String
-	{
-		return FlxG.save.data.downscroll ? "Upscroll" : "Upscroll";
-	}
+		{
+			FlxG.save.data.downscroll = !FlxG.save.data.downscroll;
+			display = updateDisplay();
+			return true;
+		}
+	
+		private override function updateDisplay():String
+		{
+			return FlxG.save.data.downscroll ? "Downscroll" : "Upscroll";
+		}
 }
 
 class GhostTapOption extends Option
